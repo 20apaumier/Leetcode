cfec0eda2231d154333c934eb03d392e5d8c366a
AJ Paumier <39337835+20apaumier@users.noreply.github.com>
2024-08-13 08:15:52 -0700

    Add project files.

---
+++ LeetcodeTests/MergeSortedArraysTests.cs
@@ -0,0 +1,17 @@
+using Leetcode;
+
+namespace LeetcodeTests
+{
+    [TestClass]
+    public class MergeSortedArraysTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            // test
+            MergeSortedArrays test = new MergeSortedArrays();
+
+        }
+    }
+}
