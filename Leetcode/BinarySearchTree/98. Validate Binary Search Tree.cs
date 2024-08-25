c3375885944faf43a9193ce43f53464d1431e398
AJ Paumier <39337835+20apaumier@users.noreply.github.com>
2024-08-25 07:49:42 -0700

    Added solution and tests for Validate BST

---
+++ Leetcode/BinarySearchTree/98. Validate Binary Search Tree.cs
@@ -0,0 +1,38 @@
+﻿using Leetcode.BinaryTreeGeneral;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.BinarySearchTree
+{
+    public class ValidateBST
+    {
+        private int? prev = null;
+
+        public bool IsValidBST(TreeNode root)
+        {
+            // in order traversal, keep prev and cur and check values
+            return InOrder(root);
+        }
+
+        public bool InOrder(TreeNode node)
+        {
+            if (node == null) { return true; }
+
+            if (InOrder(node.left) == false)
+            {
+                return false;
+            }
+
+            if (prev != null && node.val <= prev)
+            {
+                return false;
+            }
+            prev = node.val;
+
+            return InOrder(node.right);
+        }
+    }
+}
---
+++ LeetcodeTests/BinarySearchTree/ValidateBSTTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Leetcode.BinarySearchTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Leetcode.BinaryTreeGeneral;
+
+namespace Leetcode.BinarySearchTree.Tests
+{
+    [TestClass()]
+    public class ValidateBSTTests
+    {
+        [TestMethod()]
+        public void IsValidBSTTest()
+        {
+            // Arrange
+            TreeNode root = new TreeNode(2);
+            root.left = new TreeNode(1);
+            root.right = new TreeNode(3);
+
+            ValidateBST valid = new ValidateBST();
+
+            // Act
+            bool result = valid.IsValidBST(root);
+            bool expected = true;
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void IsValidBSTTest2()
+        {
+            // Arrange
+            TreeNode root = new TreeNode(5);
+            root.left = new TreeNode(1);
+            root.right = new TreeNode(4);
+            root.right.left = new TreeNode(3);
+            root.right.right = new TreeNode(6);
+
+
+            ValidateBST valid = new ValidateBST();
+
+            // Act
+            bool result = valid.IsValidBST(root);
+            bool expected = false;
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
