bae1fb72e4e307e64e18b817a86c404a3cd76cfc
AJ Paumier <39337835+20apaumier@users.noreply.github.com>
2024-08-25 05:45:05 -0700

    Added Solution to Binary Tree Right Side View

---
+++ Leetcode/Binary Tree BFS/199. Binary Tree Right Side View.cs
@@ -0,0 +1,49 @@
+﻿using Leetcode.BinaryTreeGeneral;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Binary_Tree_BFS
+{
+    public class RightSide
+    {
+        public static IList<int> RightSideView(TreeNode root)
+        {
+            // init the output list, if root is null return empty list
+            List<int> output = new List<int>();
+            if (root == null) { return output; }
+
+            // init queue of Nodes and add the root
+            Queue<TreeNode> q = new Queue<TreeNode>();
+            q.Enqueue(root);
+
+            // start bfs
+            while (q.Count != 0)
+            {
+                // the count of the queue will be the length of the current level
+                int LevelLength = q.Count;
+
+                // go through each node at the current level by dequeuing up to the length
+                for (int i = 0; i < LevelLength; i++)
+                {
+                    TreeNode node = q.Dequeue();
+
+                    // if we are at the end of the level, we are at the right hand side!!
+                    if (i == LevelLength - 1)
+                    {
+                        // add the node's value to the output
+                        output.Add(node.val);
+                    }
+
+                    // enqueue children to the queue
+                    if (node.left != null) { q.Enqueue(node.left); }
+                    if (node.right != null) { q.Enqueue(node.right); }
+                }
+            }
+
+            return output;
+        }
+    }
+}
---
+++ LeetcodeTests/Binary Tree BFS/RightSideTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Leetcode.Binary_Tree_BFS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetcodeTests.Binary_Tree_BFS;
+using System.ComponentModel;
+
+namespace Leetcode.Binary_Tree_BFS.Tests
+{
+    [TestClass()]
+    public class RightSideTests
+    {
+        [TestMethod()]
+        public void RightSideViewTest()
+        {
+            // Arrange
+            var root = BinaryTreeBFSHelper.BuildTree([1, 2, 3, null, 5, null, 4]);
+
+            // Act
+            IList<int> result = RightSide.RightSideView(root);
+            List<int> expected = new List<int> { 1, 3, 4 };
+
+            // Assert
+            CollectionAssert.AreEqual(expected, result.ToList());
+        }
+    }
+}
