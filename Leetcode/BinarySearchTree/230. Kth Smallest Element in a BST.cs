877bd0cd156072682ac01fa5423419e3f40270a9
AJ Paumier <39337835+20apaumier@users.noreply.github.com>
2024-08-25 07:24:22 -0700

    Added solution and tests for kth smallest element in a BST

---
+++ Leetcode/BinarySearchTree/230. Kth Smallest Element in a BST.cs
@@ -0,0 +1,36 @@
+﻿using Leetcode.BinaryTreeGeneral;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.BinarySearchTree
+{
+    public class KthSmallestElement
+    {
+        int count = 0;
+        int result = 0;
+
+        public int KthSmallest(TreeNode root, int k)
+        {
+            // in order traversal, keep a count, if reach k, return cur node val
+            InOrderTrav(root, k);
+            return result;
+        }
+
+        public void InOrderTrav(TreeNode node, int k)
+        {
+            if (node == null) { return; }
+
+            InOrderTrav(node.left, k);
+            count++;
+            if (count == k)
+            {
+                result = node.val;
+                return;
+            }
+            InOrderTrav(node.right, k);
+        }
+    }
+}
---
+++ LeetcodeTests/BinarySearchTree/KthSmallestElementTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Leetcode.BinarySearchTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Leetcode.BinaryTreeGeneral;
+
+namespace Leetcode.BinarySearchTree.Tests
+{
+    [TestClass()]
+    public class KthSmallestElementTests
+    {
+        [TestMethod()]
+        public void KthSmallestTest()
+        {
+            // Arrange
+            TreeNode root = new TreeNode(3);
+            root.left = new TreeNode(1);
+            root.right = new TreeNode(4);
+            root.left.right = new TreeNode(2);
+
+            KthSmallestElement kth = new KthSmallestElement();
+
+            // Act
+            int result = kth.KthSmallest(root, 1);
+            int expected = 1;
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
