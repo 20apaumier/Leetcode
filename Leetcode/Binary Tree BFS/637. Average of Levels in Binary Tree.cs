60e2e39af01dcff0759c6d0eff060dec56c9d2f3
AJ Paumier <39337835+20apaumier@users.noreply.github.com>
2024-08-25 05:48:51 -0700

    Added Average of Levels in Binary Tree Solution

---
+++ Leetcode/Binary Tree BFS/637. Average of Levels in Binary Tree.cs
@@ -0,0 +1,43 @@
+﻿using Leetcode.BinaryTreeGeneral;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Binary_Tree_BFS
+{
+    public class BinaryTreeAverageLevels
+    {
+        public static IList<double> AverageOfLevels(TreeNode root)
+        {
+            List<double> averages = new List<double>();
+            if (root == null) { return averages; }
+
+            Queue<TreeNode> q = new Queue<TreeNode>();
+            q.Enqueue(root);
+
+            while (q.Count != 0)
+            {
+                int LengthOfLevel = q.Count;
+                double LevelCount = 0.00;
+
+                for (int i = 0; i < LengthOfLevel; i++)
+                {
+                    TreeNode node = q.Dequeue();
+                    LevelCount += node.val;
+
+                    if (i == LengthOfLevel - 1)
+                    {
+                        averages.Add(LevelCount / LengthOfLevel);
+                    }
+
+                    if (node.left != null) { q.Enqueue(node.left); }
+                    if (node.right != null) { q.Enqueue(node.right); }
+                }
+            }
+
+            return averages;
+        }
+    }
+}
---
+++ LeetcodeTests/Binary Tree BFS/BinaryTreeAverageLevelsTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Leetcode.Binary_Tree_BFS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetcodeTests.Binary_Tree_BFS;
+
+namespace Leetcode.Binary_Tree_BFS.Tests
+{
+    [TestClass]
+    public class BinaryTreeAverageLevelsTests
+    {
+        [TestMethod()]
+        public void AverageOfLevelsTest()
+        {
+            // Arrange
+            var root = BinaryTreeBFSHelper.BuildTree([3, 9, 20, null, null, 15, 7]);
+
+            // Act
+            IList<double> result = BinaryTreeAverageLevels.AverageOfLevels(root);
+            List<double> expected = new List<double> { 3.00000, 14.50000, 11.00000 };
+
+            // Assert
+            CollectionAssert.AreEqual(expected, result.ToList());
+        }
+    }
+}
