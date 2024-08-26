c046baf5ffdfe0b9e25cd9c70acaf24293bef261
AJ Paumier <39337835+20apaumier@users.noreply.github.com>
2024-08-26 09:20:38 -0700

    added solution and tests for remove duplicates from sorted array II

---
+++ Leetcode/ArrayString/80. Remove Duplicates from Sorted Array II.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.ArrayString
+{
+    public class RemoveDupesII
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            if (nums.Length < 3) { return nums.Length; }
+            int insertIntoArray = 2;
+
+            for (int i = 2; i < nums.Length; i++)
+            {
+                if (nums[i] != nums[insertIntoArray - 2])
+                {
+                    nums[insertIntoArray] = nums[i];
+                    insertIntoArray++;
+                }
+            }
+
+            return insertIntoArray;
+        }
+    }
+}
---
+++ LeetcodeTests/ArrayString/RemoveDupesIITests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Leetcode.ArrayString;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.ArrayString.Tests
+{
+    [TestClass()]
+    public class RemoveDupesIITests
+    {
+        [TestMethod()]
+        public void RemoveDuplicatesTest()
+        {
+            // Arrange
+            int[] nums = [1, 1, 1, 2, 2, 3];
+
+            RemoveDupesII rd = new RemoveDupesII();
+
+            // Act
+            int result = rd.RemoveDuplicates(nums);
+            int expected = 5;
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void RemoveDuplicatesTest2()
+        {
+            // Arrange
+            int[] nums = [0, 0, 1, 1, 1, 1, 2, 3, 3];
+
+            RemoveDupesII rd = new RemoveDupesII();
+
+            // Act
+            int result = rd.RemoveDuplicates(nums);
+            int expected = 7;
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
