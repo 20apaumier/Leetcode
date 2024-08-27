b0d4166526acf252c8db653905faee54a3016056
AJ Paumier <39337835+20apaumier@users.noreply.github.com>
2024-08-27 08:23:04 -0700

    Added solution and tests  to H-Index problem

---
+++ Leetcode/ArrayString/274. H-Index.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.ArrayString
+{
+    public class HInd
+    {
+        public int HIndex(int[] citations)
+        {
+            Array.Sort(citations);
+
+            for (int i = 0; i < citations.Length; i++)
+            {
+                if (citations[i] >= citations.Length - i)
+                {
+                    return citations.Length - i;
+                }
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ LeetcodeTests/ArrayString/HIndTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Leetcode.ArrayString;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.ArrayString.Tests
+{
+    [TestClass()]
+    public class HIndTests
+    {
+        [TestMethod()]
+        public void HIndexTest()
+        {
+            // Arrange
+            int[] citations = [3, 0, 6, 1, 5];
+            HInd ind = new HInd();
+
+            // Act
+            int result = ind.HIndex(citations);
+            int expected = 3;
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void HIndexTest2()
+        {
+            // Arrange
+            int[] citations = [1, 3, 1];
+            HInd ind = new HInd();
+
+            // Act
+            int result = ind.HIndex(citations);
+            int expected = 1;
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
