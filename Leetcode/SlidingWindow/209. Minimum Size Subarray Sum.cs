3be881edbb8c89e0df73fab6526bacc91984afed
AJ Paumier <39337835+20apaumier@users.noreply.github.com>
2024-09-09 13:24:39 -0700

    added minimum size subarray solution and test

---
+++ Leetcode/SlidingWindow/209. Minimum Size Subarray Sum.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.SlidingWindow
+{
+    public class MinSizeSub
+    {
+        public int MinSubArrayLen(int target, int[] nums)
+        {
+            int left = 0;
+            int right = 0;
+            int output = 0;
+
+            int curWindow = nums[0];
+            while (right <= nums.Length)
+            {
+                if (curWindow >= target)
+                {
+                    if (output == 0) { output = right - left + 1; }
+                    else { output = Math.Min(output, right - left + 1); }
+                    curWindow -= nums[left];
+                    left++;
+                }
+                else
+                {
+                    right++;
+                    if (right == nums.Length) { break; }
+                    curWindow += nums[right];
+                }
+            }
+
+            return output;
+        }
+    }
+}
---
+++ LeetcodeTests/SlidingWindow/MinSizeSubTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Leetcode.SlidingWindow;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.SlidingWindow.Tests
+{
+    [TestClass()]
+    public class MinSizeSubTests
+    {
+        [TestMethod()]
+        public void MinSubArrayLenTest()
+        {
+            // Arrange
+            int target = 7;
+            int[] nums = [2, 3, 1, 2, 4, 3];
+
+            // Act
+            MinSizeSub mss = new MinSizeSub();
+            int expected = 2;
+            int result = mss.MinSubArrayLen(target, nums);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
