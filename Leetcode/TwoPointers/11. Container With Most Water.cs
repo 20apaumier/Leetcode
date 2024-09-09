ad785442b841301e4fab72cbccaaeda1c8b336d7
AJ Paumier <39337835+20apaumier@users.noreply.github.com>
2024-09-09 12:48:23 -0700

    added container with most water problem solution and tests

---
+++ Leetcode/TwoPointers/11. Container With Most Water.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.TwoPointers
+{
+    public class MostWater
+    {
+        public int MaxArea(int[] height) {
+            // two pointers
+            // move the smaller of the two values at each iteration
+
+            int left = 0;
+            int right = height.Length - 1;
+            int maxWater = 0;
+
+            while (left < right) {
+                maxWater = Math.Max(maxWater, Math.Min(height[left], height[right]) * (right - left));
+            
+                if (height[left] <= height[right]) {
+                    left++;
+                } else { 
+                    right--;
+                }
+            }
+
+            return maxWater;
+        }
+    }
+}
---
+++ LeetcodeTests/TwoPointers/MostWaterTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Leetcode.TwoPointers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.TwoPointers.Tests
+{
+    [TestClass()]
+    public class MostWaterTests
+    {
+        [TestMethod()]
+        public void MaxAreaTest()
+        {
+            // Arrange
+            int[] height = [1, 8, 6, 2, 5, 4, 8, 3, 7];
+            MostWater mw = new MostWater();
+
+            // Act
+            int expected = 49;
+            int result = mw.MaxArea(height);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void MaxAreaTest2()
+        {
+            // Arrange
+            int[] height = [2, 3, 4, 5, 18, 17, 6];
+            MostWater mw = new MostWater();
+
+            // Act
+            int expected = 17;
+            int result = mw.MaxArea(height);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
