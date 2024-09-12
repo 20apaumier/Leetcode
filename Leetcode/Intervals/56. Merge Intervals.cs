ea4830dbdfbf0f122427b867ec82193c8b14ec29
AJ Paumier <39337835+20apaumier@users.noreply.github.com>
2024-09-12 14:22:54 -0700

    Added Merge Intervals solution and tests

---
+++ Leetcode/Intervals/56. Merge Intervals.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Intervals
+{
+    public class MergeIntervals
+    {
+        public int[][] Merge(int[][] intervals)
+        {
+            // sort the input by the first item in the inner array
+            Array.Sort(intervals, (a, b) => a[0].CompareTo(b[0]));
+
+            List<int[]> output = new List<int[]>();
+            int[] prev = intervals[0];
+            
+            for (int i = 1; i < intervals.Length; i++)
+            {
+                int[] interval = intervals[i];
+                if (interval[0] <= prev[1])
+                {
+                    // merge
+                    prev[1] = Math.Max(prev[1], interval[1]);
+                } else
+                {
+                    output.Add(prev);
+                    prev = interval;
+                }
+            }
+
+            output.Add(prev);
+            return output.ToArray();
+        }
+    }
+}
---
+++ LeetcodeTests/Intervals/MergeIntervalsTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Leetcode.Intervals;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Intervals.Tests
+{
+    [TestClass()]
+    public class MergeIntervalsTests
+    {
+        [TestMethod()]
+        public void MergeTest()
+        {
+            // Arrange
+            int[][] intervals = [[1, 3], [2, 6], [8, 10], [15, 18]];
+            MergeIntervals mi = new MergeIntervals();
+
+            // Act
+            int[][] expected = [[1, 6], [8, 10], [15, 18]];
+            int[][] result = mi.Merge(intervals);
+
+            // Assert
+            for (int i = 0; i < result.Length; i++)
+            {
+                CollectionAssert.AreEqual(expected[i], result[i]);
+            }
+            
+        }
+
+        [TestMethod()]
+        public void MergeTest2()
+        {
+            // Arrange
+            int[][] intervals = [[1, 4], [4, 5]];
+            MergeIntervals mi = new MergeIntervals();
+
+            // Act
+            int[][] expected = [[1, 5]];
+            int[][] result = mi.Merge(intervals);
+
+            // Assert
+            for (int i = 0; i < result.Length; i++)
+            {
+                CollectionAssert.AreEqual(expected[i], result[i]);
+            }
+        }
+    }
+}
